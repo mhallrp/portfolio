a6e09e451f9e6237031337d8c7f396e70c363fbe
“Matt” <mhallrp@gmail.com>
2024-01-21 18:48:36 +0100

    portfolio98

---
+++ Components/TestC/TestC.tsx
@@ -0,0 +1,7 @@
+
+const TestC = () =>{
+    return (
+        <div><h1>HELLO WORLD</h1></div>
+    )
+}
+export default TestC
--- app/page.tsx
+++ app/page.tsx
@@ -1,37 +1,42 @@
-"use client";
+
 import Desktop from "../Components/Desktop";
-const apiKey = process.env.API_KEY;
-async function getSessionStatus() {
-  if (typeof apiKey !== "undefined") {
-    try {
-      const response = await fetch(`https://request.matt-hall.dev/check`, {
-        credentials: "include",
-        headers: {
-          "X-API-Key": apiKey,
-        },
-      });
-      const data = await response.json();
-      if (response.ok) {
-        console.log("response is ok");
-        return { data: data, success: true };
-      } else {
-        console.log("response error");
-        return { data: data.error, success: false };
-      }
-    } catch (networkError) {
-      console.log("network error");
-      return { data: "Network error occurred", success: false };
-    }
-  }
-  console.log("api key is bad");
-  return { data: "API key is undefined", success: false };
-}
+import TestC from "@/Components/TestC";
+// const apiKey = process.env.API_KEY;
+// async function getSessionStatus() {
+//   if (typeof apiKey !== "undefined") {
+//     try {
+//       const response = await fetch(`https://request.matt-hall.dev/check`, {
+//         credentials: "include",
+//         headers: {
+//           "X-API-Key": apiKey,
+//         },
+//       });
+//       const data = await response.json();
+//       if (response.ok) {
+//         console.log("response is ok");
+//         return { data: data, success: true };
+//       } else {
+//         console.log("response error");
+//         return { data: data.error, success: false };
+//       }
+//     } catch (networkError) {
+//       console.log("network error");
+//       return { data: "Network error occurred", success: false };
+//     }
+//   }
+//   console.log("api key is bad");
+//   return { data: "API key is undefined", success: false };
+// }
 
 const  page = () => {
   // const data = await getSessionStatus();
   return (
     <div className={`flex h-dvh w-screen overflow-hidden bg-windoorsGreen`}>
-      <Desktop initialUserData={ { name: "", score: 0 } } initialState={"login"} />
+
+<TestC />
+
+
+      {/* <Desktop initialUserData={ { name: "", score: 0 } } initialState={"login"} /> */}
     </div>
   );
 }
