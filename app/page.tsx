ec23e23a574874e12c01ef5dbbf4ce4e4437b857
“Matt” <mhallrp@gmail.com>
2024-01-23 16:16:18 +0100

    session check

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import React, { useState, useEffect } from "react";
 import { getSessionStatus } from "@/Model/authOutcalls";
 import Desktop from "../Components/Desktop";
@@ -17,24 +16,26 @@ interface SessionResponse {
 export default function App() {
   const [sessionData, setSessionData] = useState<SessionResponse>({ success: false, data: { name: "", score: 0 } });
   const [state, changeState] = useState("login");
+  const [checked, setChecked] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
       const response = await getSessionStatus();
       setSessionData(response);
-      response.success && changeState("quiz")
+      response.success && changeState("quiz");
     };
-
     fetchData();
   }, []);
 
   useEffect(() => {
-    console.log(sessionData.success);
+    setChecked(true);
   }, [sessionData]);
 
   return (
-    <div className={`flex h-dvh w-screen overflow-hidden bg-white`}>
-      <Desktop initialUserData={{ name: sessionData.data.name, score: sessionData.data.score }} state={state} changeState={changeState} />
-    </div>
+    checked && (
+      <div className={`flex h-dvh w-screen overflow-hidden bg-white`}>
+        <Desktop initialUserData={{ name: sessionData.data.name, score: sessionData.data.score }} state={state} changeState={changeState} />
+      </div>
+    )
   );
 }
