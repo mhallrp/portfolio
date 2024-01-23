50213a32b25bfe54d1ac7231e0c22ad3cad3e6cf
“Matt” <mhallrp@gmail.com>
2024-01-23 14:22:19 +0100

    server functions

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,29 @@
 import Desktop from "../Components/Desktop";
 
-async function getSessionStatus() {
-  const apiKey = process.env.NEXT_PUBLIC_API_KEY;
-  if (typeof apiKey !== "undefined") {
-    try {
-      const response = await fetch(`https://request.matt-hall.dev/check`, {
-        credentials: "include",
-        headers: { "X-API-Key": apiKey },
-      });
-      const data = await response.json();
-      return { 
-        success: response.ok,
-        data: data.data || data.message
-      };
-    } catch (networkError) {
-      return { success: false, data: "Network error occurred" };
-    }
-  }
-  return { success: false, data: "API key is undefined" };
-}
+
 
 export default async function App() {
+  async function getSessionStatus() {
+    'use server'
+    const apiKey = process.env.NEXT_PUBLIC_API_KEY;
+    if (typeof apiKey !== "undefined") {
+      try {
+        const response = await fetch(`https://request.matt-hall.dev/check`, {
+          credentials: "include",
+          headers: { "X-API-Key": apiKey },
+        });
+        const data = await response.json();
+        return { 
+          success: response.ok,
+          data: data.data || data.message
+        };
+      } catch (networkError) {
+        return { success: false, data: "Network error occurred" };
+      }
+    }
+    return { success: false, data: "API key is undefined" };
+  }
+
   const { success, data } = await getSessionStatus();
   console.log("success?: " + success);
   console.log("data: " + data);
