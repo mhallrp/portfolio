0afbfc58ae0486757a71d0643ba3231a78bda61c
“Matt” <mhallrp@gmail.com>
2024-01-23 13:07:37 +0100

    server function update

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,24 @@
 import Desktop from "../Components/Desktop";
-
-const apiKey = process.env.API_KEY;
-
 async function getSessionStatus() {
-  if (typeof apiKey !== "undefined") {
-    try {
-      const response = await fetch(`https://request.matt-hall.dev/check`, {
-        credentials: "include",
-        headers: {
-          "X-API-Key": apiKey,
-        },
-      });
-      const data = await response.json();
-      if (response.ok) {
-        return { data: data, success: true };
-      } else {
-        return { data: data.error, success: false };
-      }
-    } catch (networkError) {
-      return { data: "Network error occurred", success: false };
-    }
+  const response = await fetch(`https://request.matt-hall.dev/check`, {
+    credentials: "include",
+    headers: {
+      "X-API-Key": process.env.NEXT_PUBLIC_API_KEY as string,
+    },
+  });
+  const data = await response.json();
+  if (response.ok) {
+    return { data: data, success: true };
+  } else {
+    return { data: data.error, success: false };
   }
-  return { data: "API key is undefined", success: false };
 }
-
 export default async function App() {
   const data = await getSessionStatus();
+  console.log(data.data);
   return (
     <div className={`flex h-dvh w-screen overflow-hidden bg-black`}>
-      <Desktop initialUserData={ data.success ? { name: data.data.name, score: data.data.score } : { name: "", score: 0 } } initialState={"login"} />
+      <Desktop initialUserData={data.success ? { name: data.data.name, score: data.data.score } : { name: "", score: 0 }} initialState={"login"} />
     </div>
   );
 }
